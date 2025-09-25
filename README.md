0130ea7dca1b7b2aef32a6a8f557887a0fa2d82f
zxc-w <163780208+zxc-w@users.noreply.github.com>
2025-09-25 17:26:37 +0300

    deploy project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Bar Chart Visualization
+
+Project #1 of the Data Visualization using D3 library Certification on freeCodeCamp.
+
+## What I learned
+
+- The `timeScale()` in D3 to create `xScale` function for time on the horizontal axis.
+
+- The `d3.parseDate()` method to parse the data object into a specific string format.
+
+- Improved my understanding of the `domain()` and `range()` methods on the scales, where I first thought that the range is the value of y axis for the horizontal axis or the value of x axis for the vertical axis. The range actually was the output value of the raw datum to where it should be on the available width of `svg`.
+
+- Leanred the `.on()` event handler on D3 and `.html()` to insert HTML code into a selected element.
+
+## Live Preview
+
+Check out the live preview: [Bar Chart](https://zxc-w.github.io/Bar-Chart/).
---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import * as d3 from "d3";
+import "./App.css";
+
+function App() {
+  const [dataset, setDataset] = useState([]);
+  const formatDate = d3.timeFormat("%Y-%m-%d");
+  const svg = d3.select("svg");
+  const tooltip = d3.select("#tooltip");
+  const padding = 50;
+  const w = 1000;
+  const h = 600;
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        const parseDate = d3.timeParse("%Y-%m-%d");
+        const data = res.data.map((d) => [parseDate(d[0]), d[1]]);
+        setDataset(data);
+      });
+  }, []);
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(dataset, (d) => d[0]))
+    .range([padding, w - padding]);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d[1])])
+    .range([h - padding, padding]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  svg.attr("width", w).attr("height", h);
+
+  svg
+    .append("g")
+    .attr("id", "x-axis")
+    .attr("transform", `translate(0, ${h - padding})`)
+    .call(xAxis);
+
+  svg
+    .append("g")
+    .attr("id", "y-axis")
+    .attr("transform", `translate(${padding}, 0)`)
+    .call(yAxis);
+
+  svg
+    .selectAll("rect")
+    .data(dataset)
+    .join("rect")
+    .attr("class", "bar")
+    .attr("data-date", (d) => formatDate(d[0]))
+    .attr("data-gdp", (d) => d[1])
+    .attr("x", (d) => xScale(d[0]))
+    .attr("y", (d) => yScale(d[1]))
+    .attr("width", w / dataset.length)
+    .attr("height", (d) => h - padding - yScale(d[1]))
+    .on("mouseover", (event, d) => {
+      tooltip
+        .style("opacity", 1)
+        .attr("data-date", formatDate(d[0]))
+        .style("left", event.pageX + 10 + "px")
+        .style("top", event.pageY - 40 + "px")
+        .html(`${formatDate(d[0])}<br/>$${d[1].toString()} Billion`);
+    })
+    .on("mouseout", () => tooltip.style("opacity", 0));
+  return (
+    <>
+      <h1 id="title">U.S.A GDP</h1>
+      <svg></svg>
+      <div id="tooltip"></div>
+    </>
+  );
+}
+
+export default App;
